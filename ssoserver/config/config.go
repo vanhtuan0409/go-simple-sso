7bb1fc0f9f4e3dd9e76cabf38b7247ab8ff1c0fc
Tuấn Vương <vanhtuan0409@gmail.com>
2018-03-09 16:00:57 +0700

    add default and export docker port

--- ssoserver/config/config.go
+++ ssoserver/config/config.go
@@ -4,7 +4,7 @@ import "github.com/caarlos0/env"
 
 type Config struct {
 	DefaultCallback string `env:"DEFAULT_CALLBACK"`
-	HTTP_PORT       int    `env:"HTTP_PORT"`
+	HTTP_PORT       int    `env:"HTTP_PORT" envDefault:"5000"`
 }
 
 func Parse() *Config {
--- web/config/config.go
+++ web/config/config.go
@@ -5,8 +5,8 @@ import "github.com/caarlos0/env"
 type Config struct {
 	SSS_URL    string `env:"SSO_URL"`
 	SERVER_URL string `env:"SERVER_URL"`
-	HTTP_PORT  int    `env:"HTTP_PORT"`
-	APP_TITLE  string `env:"APP_TITLE"`
+	HTTP_PORT  int    `env:"HTTP_PORT" envDefault:"8080"`
+	APP_TITLE  string `env:"APP_TITLE" envDefault:"Web Client"`
 }
 
 func Parse() *Config {
